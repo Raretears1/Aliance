29dbbd76887fcc626cffc4db65493ef2d2d3201b
Владислав <77351191+Raretears1@users.noreply.github.com>
2021-08-22 16:01:49 +0300

    Add files via upload

---
+++ aliance1/prod_settings.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-yfl1$ax779wo*dsg462wfsw((&3%3)vgbp12mv+t*j!w+w8+wg2v7'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
